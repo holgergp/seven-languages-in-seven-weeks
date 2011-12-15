a83f452375222ab244230a64f0df763eef31d0fc
Holger Plankermann <holgergp@gmail.com>
2011-12-15 08:24:25 +0100

    beautifying and comments

--- ruby/day2/arrayPrint.rb
+++ ruby/day2/arrayPrint.rb
@@ -1,21 +1,36 @@
-def arraySimplePrint(a)
 
-i=0
+# Iterate over an array of 16 numbers using just 'each'
+# Print 4 numbers each in one line
+def arraySimplePrint(a)
+puts "Iterate over an array using 'each'."
+# I need somethin that counts the current array index
+# Well, thats more than just 'each'. Perhaps there is a better solution for this
+i=1
 a.each do |value| 
   print value
-  print "\n" if (i+=1)%4==0
+  # Use mod to create a new line
+  if i%4==0
+    print "\n" 
+  else
+    # To make it look prettier
+    print ","		
+  end
+  i+=1
+end
 end
 
-end
-
+# Iterate over an array of 16 numbers using just 'each_slice' of enumarable
 def arraySlicePrint(a)
-a.each_slice(4) {|value| p value }
+puts "Iterate over an array using 'each_slice'."
+a.each_slice(4) {|value| puts value.join(',') }
 
 end
 
 
-a=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+a=(1..16).to_a
+
 
 arraySimplePrint a
+puts "-------------------------------------------"
 arraySlicePrint a
 
--- ruby/day2/hashConvert.rb
+++ ruby/day2/hashConvert.rb
@@ -1,4 +1,4 @@
-
+#Convert an array to a hash
 def a_t_h
 puts "Array to Hash"
 a1=["test","this",1,"feature"]
@@ -6,12 +6,14 @@ puts a1
 puts Hash[*a1.flatten]
 end
 
+#Convert a hash to an array
 def h_t_a
 puts "Hash to Array"
 h1={:first=>"test",:second=>"this",:third=>"feature"}
 h1.to_a.each{|value|puts value}
 end
 
+#Iterate over a hash
 def iterate
 puts "Iterate"
 {:first=>"test",:second=>"this",:third=>"feature"}.each {|key,value| puts "Key: #{key} Value: #{value}"}
