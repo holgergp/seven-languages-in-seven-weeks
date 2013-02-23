06e1d2bb9224f30820785e3e699abc5325bc113f
holgergp <holgergp@gmail.com>
2013-02-23 19:55:31 +0100

    Ein wenig Refactoring

--- scala/day1/Tictactoe.scala
+++ scala/day1/Tictactoe.scala
@@ -2,29 +2,24 @@ object Tictactoe extends App {
   override def main(args: Array[String]) {
 
   	var board= new Board
+  	val player1 = new Player("X");
+  	val player2 = new Player("O");
+
   	board.printBoard
 
   	 while(!board.isFinished){
   		var succesful1=false
   		while(!succesful1) {
-	  		println("Spieler X: ")
-	  		println("XPos: ")
-	  		var xPos1=readLine();
-	  		println("YPos: ")
-	  		var yPos1=readLine();
-	  		succesful1= board.putXOn(xPos1.toInt,yPos1.toInt)	;
+  			val (xPos,yPos)=player1.makeAMove()
+	  		succesful1= board.putXOn(xPos.toInt,yPos.toInt)	;
   		}
   		board.printBoard
 
   		if(!board.isFinished) {
   			var succesful2=false
   			while(!succesful2) {
-	  			println("Spieler Y: ")
-	  			println("XPos: ")
-	  			var xPos2=readLine();
-	  			println("YPos: ")
-	  			var yPos2=readLine();
-	  			succesful2= board.putOOn(xPos2.toInt,yPos2.toInt);
+  				val (xPos,yPos)=player2.makeAMove()
+	  			succesful2= board.putOOn(xPos.toInt,yPos.toInt);
   			}
   			board.printBoard
   		}		
@@ -39,9 +34,22 @@ object Tictactoe extends App {
 
   	else println ("Unentschieden!")
     	
-
   }
 
+}
+
+class Player(val playerCharacter:String) {
+
+
+
+	def makeAMove(): (String,String) ={
+	  		println("Spieler " +playerCharacter+": ")
+	  		println("XPos: ")
+	  		var xPos=readLine();
+	  		println("YPos: ")
+	  		var yPos=readLine();
+	  		return (xPos,yPos)
+	}
 
 
 }
